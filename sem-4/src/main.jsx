81016226772fa6cef3ab673e8ef75f8dcc335526
Vyakhya Goyal <vyakhyatwentytwo@gmail.com>
2026-02-04 09:34:12 +0530

    class component example

--- sem-4/src/main.jsx
+++ sem-4/src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
+import React, { Component } from 'react';
+
+let m="vivo"
+
+//class component example
+export default class Mobile extends Component{
+  render(){
+    return(
+      <h1>I have {this.props.brand}</h1>
+    )
+  }
+}
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>,
+  <Mobile brand={m}/>
 )
+
+
